2e8a808d88694ead89bf36f021c4ed37d8d98e06
githubch <944214536@qq.com>
2015-05-11 10:46:00 +0800

    [Jun] update code

--- DDDScrumFacts/ProductFact.cs
+++ DDDScrumFacts/ProductFact.cs
@@ -6,7 +6,7 @@ namespace DDDScrumFacts
     public class ProductFact
     {
         [Fact]
-        public void should_plan_a_back_log_item_to_a_product()
+        public void should_rename_a_product()
         {
             var product = new Product("ScrumDDD");
             product.Rename("NewScrumDDD");
