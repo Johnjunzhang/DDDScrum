69194acdf8e6777792adb70fc918ef1f17ec4be1
githubch <944214536@qq.com>
2015-05-11 21:26:49 +0800

    [Jun] add notification

---
+++ DDDNotification/FakeMessageSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DDDNotification
+{
+    public class FakeMessageSender
+    {
+        public static string Message { get; private set; }
+
+        public static void Send(String backLogItemName, String sprintName)
+        {
+            Message = string.Format("backLogItem:[{0}] has been committed to sprint:[{1}]", backLogItemName, sprintName);
+        }
+    }
+}
