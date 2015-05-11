a9147467fdba8ecdb55acb6ed020f31b8e1aa982
githubch <944214536@qq.com>
2015-05-11 10:42:33 +0800

    [Jun] Init Code

---
+++ DDDScrum/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DDDScrum
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+
+        public void Rename(String newName)
+        {
+            Name = newName;
+        }
+    }
+}
---
+++ DDDScrumFacts/ProductFact.cs
@@ -0,0 +1,17 @@
+﻿using DDDScrum;
+using Xunit;
+
+namespace DDDScrumFacts
+{
+    public class ProductFact
+    {
+        [Fact]
+        public void should_plan_a_back_log_item_to_a_product()
+        {
+            var product = new Product("ScrumDDD");
+            product.Rename("NewScrumDDD");
+
+            Assert.Equal(product.Name, "NewScrumDDD");
+        }
+    }
+}
