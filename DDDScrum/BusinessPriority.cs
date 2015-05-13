95f76440f695599eaf3db9bc928bcf23fc4718dc
githubch <944214536@qq.com>
2015-05-13 16:48:50 +0800

    [Jun] add equal method

--- DDDScrum/BusinessPriority.cs
+++ DDDScrum/BusinessPriority.cs
@@ -19,5 +19,29 @@ namespace DDDScrum
         {
             get { return Math.Round((value * 3 + risk * 2) / (cost * 3), 2); }
         }
+
+        protected bool Equals(BusinessPriority other)
+        {
+            return value.Equals(other.value) && risk.Equals(other.risk) && cost.Equals(other.cost);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((BusinessPriority) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = value.GetHashCode();
+                hashCode = (hashCode*397) ^ risk.GetHashCode();
+                hashCode = (hashCode*397) ^ cost.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 }
